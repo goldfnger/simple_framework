e41ded6bf14eef80079321a1efcd40817a941136
Aleksandr Kornjushko <>
2023-04-25 14:16:31 +0300

    create styles helper

---
+++ helpers/styles.py
@@ -0,0 +1,50 @@
+class Colors:
+    """Colors"""
+
+    LIGHT_BLUE = "rgba(35, 161, 209, 1)"
+    DARK_BLUE = "rgba(35, 82, 124, 1)"
+    WHITE = "rgba(255, 255, 255, 1)"
+    DARK_GRAY = "rgba(54, 54, 54, 1)"
+    BLACK = "rgba(0, 0, 0, 1)"
+    LIGHT_BLACK = "rgba(34, 34, 34, 1)"
+    LIGHT_GRAY = "rgba(102, 102, 102, 1)"
+    MEDIUM_GRAY = "rgba(68, 68, 68, 1)"
+    GRAY_136 = 'rgba(136, 136, 136, 1)'
+    GRAY_238 = 'rgba(238, 238, 238, 1)'
+    GRAY_68 = 'rgba(68, 68, 68, 1)'
+    GRAY_221 = 'rgba(221, 221, 221, 1)'
+
+
+class Gradients:
+    """Gradients"""
+
+    LIGHT_BLACK = 'linear-gradient(rgb(68, 68, 68), rgb(34, 34, 34))'
+    MEDIUM_BLACK = 'linear-gradient(rgb(51, 51, 51), rgb(17, 17, 17))'
+
+
+class Border:
+    """Frames"""
+
+    LIGHT_GRAY = '1px solid rgb(230, 230, 230)'
+
+
+class Cursor:
+    """Cursor styles"""
+
+    POINTER = 'pointer'
+
+
+class FontWeight:
+    """Font style"""
+
+    WEIGHT_700 = '700'
+    WEIGHT_500 = '500'
+
+
+class SIZES:
+    """Font size"""
+
+    SIZE_33 = '33px'
+    SIZE_12 = '12px'
+    SIZE_18 = '18px'
+    SIZE_4 = '4px'
